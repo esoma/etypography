e735cdf074233a4502d6a1db837d30094445d431
Erik Soma <stillusingirc@gmail.com>
2024-03-12 21:48:05 -0400

    Add character_is_normally_rendered.

---
+++ src/etext/__init__.py
@@ -0,0 +1,4 @@
+__all__ = ["character_is_normally_rendered"]
+
+# etext
+from ._unicode import character_is_normally_rendered
---
+++ test/test_unicode.py
@@ -0,0 +1,28 @@
+# etext
+from etext import character_is_normally_rendered
+
+# pytest
+import pytest
+
+
+@pytest.mark.parametrize(
+    "character, result",
+    [
+        (" ", True),
+        ("\n", False),
+        ("\t", False),
+        ("\r", False),
+        ("\u2028", True),
+        ("\u2029", True),
+        ("a", True),
+        ("A", True),
+        ("食", True),
+    ],
+)
+def test_character_is_normally_rendered(character, result):
+    assert character_is_normally_rendered(character) == result
+
+
+def test_character_normally_rendered_too_many():
+    with pytest.raises(TypeError) as excinfo:
+        character_is_normally_rendered("ab")
