767fd2287afcb2a34be4697c28fbaf096b8e790e
Erik Soma <stillusingirc@gmail.com>
2024-07-08 10:25:51 -0400

    homebrew repo

--- src/etypography/_break_text.py
+++ src/etypography/_break_text.py
@@ -11,7 +11,7 @@ from ctypes import c_int
 from ctypes import c_int32
 from ctypes import c_int64
 from ctypes import c_void_p
-from pathlib import Path
+import os
 from platform import system
 import subprocess
 from typing import Any
@@ -46,15 +46,9 @@ if system() == "Windows":
     _POSTFIX = ""
 else:
     if system() == "Darwin":
-        _BREW_ICU4C = Path("/usr/local/Cellar/icu4c")
-        if _BREW_ICU4C.exists():
-            _LATEST_BREW_ICU4C_VERSION = sorted(v.name for v in _BREW_ICU4C.iterdir())[-1]
-            _LATEST_BREW_ICU4C = _BREW_ICU4C / _LATEST_BREW_ICU4C_VERSION
-            _LIB_NAME = str(_LATEST_BREW_ICU4C / "lib/libicuuc.dylib")
-            _UCONV_NAME = str(_LATEST_BREW_ICU4C / "bin/uconv")
-        else:
-            _LIB_NAME = "/usr/local/opt/icu4c/lib/libicuuc.dylib"
-            _UCONV_NAME = "/usr/local/opt/icu4c/bin/uconv"
+        _HOMEBREW_REPOSITORY = os.environ.get("HOMEBREW_REPOSITORY", "/usr/local")
+        _LIB_NAME = f"{_HOMEBREW_REPOSITORY}/opt/icu4c/lib/libicuuc.dylib"
+        _UCONV_NAME = f"{_HOMEBREW_REPOSITORY}/opt/icu4c/bin/uconv"
     else:
         _LIB_NAME = "libicuuc.so"
         _UCONV_NAME = "uconv"
